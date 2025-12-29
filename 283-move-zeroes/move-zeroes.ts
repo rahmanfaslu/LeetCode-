1b266f6add980532e5004ede9a70afc522792ed0
Faslu rahman <rahmanfaslu33@gmail.com>
2025-12-29 16:59:23 +0530

    Time: 0 ms (100.00%) | Memory: 61.5 MB (23.51%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.ts
@@ -0,0 +1,15 @@
+
+function moveZeroes(nums: number[]): void {
+    let pos = 0;
+
+    for (let i =0; i<nums.length ;i++ ){
+
+        if(nums[i]!==0 ){
+            nums[pos] = nums[i]
+            pos++;
+        }
+    }
+    for(let i=pos ;i<nums.length;i++){
+        nums[i]=0;
+    }
+};
